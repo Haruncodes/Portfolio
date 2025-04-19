e3362c13fcd1b6f1b9679d07feaed7b5f2c61973
Ravi Harun <raviaharun@gmail.com>
2025-04-18 23:24:49 -0700

    lab 3

---
+++ global.js
@@ -0,0 +1,94 @@
+console.log('IT’S ALIVE!');
+
+
+function $$(selector, context = document) {
+    return Array.from(context.querySelectorAll(selector));
+  }
+
+const navLinks = $$("nav a");
+const currentLink = navLinks.find(
+  (a) => a.host === location.host && a.pathname === location.pathname
+);
+
+currentLink?.classList.add("current");
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/Portfolio/";         // My GitHub repo name
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'resume/', title: 'Resume' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'https://github.com/Haruncodes', title: 'GitHub' }
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+  
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+  
+    url = !url.startsWith('http') ? BASE_PATH + url : url;
+  
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+  
+    if (a.host === location.host && a.pathname === location.pathname) {
+      a.classList.add('current');
+    }
+  
+    if (a.host !== location.host) {
+      a.target = '_blank';
+    }
+  
+    nav.append(a);
+  }
+  document.body.insertAdjacentHTML(
+    'afterbegin',
+    `
+    <label class="color-scheme">
+      Theme:
+      <select>
+        <option value="light dark">Automatic</option>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+      </select>
+    </label>`
+);
+
+  const select = document.querySelector('.color-scheme select');
+
+  function setColorScheme(value) {
+    document.documentElement.style.setProperty("color-scheme", value);
+  }
+    if ("colorScheme" in localStorage) {
+    setColorScheme(localStorage.colorScheme);
+    select.value = localStorage.colorScheme;
+  }
+    select.addEventListener("input", (event) => {
+    const value = event.target.value;
+    setColorScheme(value);
+    localStorage.colorScheme = value;
+    console.log("color scheme changed to", value);
+  });
+
+//   const form = document.querySelector("form");
+
+// form?.addEventListener("submit", (event) => {
+//   event.preventDefault(); 
+
+//   const data = new FormData(form);
+//   const params = [];
+
+//   for (let [name, value] of data) {
+//     params.push(`${name}=${encodeURIComponent(value)}`);
+//   }
+
+//   const url = `${form.action}?${params.join("&")}`;
+
+//   location.href = url; 
+// });
