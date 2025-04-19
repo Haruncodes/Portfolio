6b5a1ef1df026b160e0747b281ddca6fb8f5b205
Ravi Harun <raviaharun@gmail.com>
2025-04-18 23:50:50 -0700

    Update global.js
    lab 3 v3

--- global.js
+++ global.js
@@ -77,20 +77,3 @@ for (let p of pages) {
     localStorage.colorScheme = value;
     console.log("color scheme changed to", value);
   });
-
-//   const form = document.querySelector("form");
-
-// form?.addEventListener("submit", (event) => {
-//   event.preventDefault(); 
-
-//   const data = new FormData(form);
-//   const params = [];
-
-//   for (let [name, value] of data) {
-//     params.push(`${name}=${encodeURIComponent(value)}`);
-//   }
-
-//   const url = `${form.action}?${params.join("&")}`;
-
-//   location.href = url; 
-// });
